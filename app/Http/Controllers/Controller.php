923671b6080a8dd6bdb1841b156211f0e179d986
olakehinde <olakehinde59@gmail.com>
2023-01-12 08:46:18 +0100

    added validation module

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,26 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function validatePayload(Request $request)
+    {
+        $rules = [];
+        $validationMessages = [];
+        $payload = $request->all();
+
+        foreach ($payload as $key => $value) {
+            $rules[$key] = $value['rules'];
+            $validationMessages[$key . '.alpha'] = 'The ' . $key . ' field must be a string.';
+            $validationMessages[$key . '.required'] = 'The ' . $key . ' field is required.';
+            $validationMessages[$key . '.email'] = 'The ' . $key . ' field must be a valid email address.';
+            $validationMessages[$key . '.number'] = 'The ' . $key . ' field must be a number.';
+        }
+
+        $validator = Validator::make($payload, $rules, $validationMessages);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        } else {
+            return response()->json(['status' => true]);
+        }
+    }
 }
